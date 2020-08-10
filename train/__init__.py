01223ee6d018f9373944c81027ba6648365f5e31
cedar <cedarjoy@outlook.com>
2020-08-10 18:29:07 +0800

    test db

--- train/__init__.py
+++ train/__init__.py
@@ -1,12 +1,13 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
+from SQLAlchemy import text
 from flask import Flask
+
 
 app = Flask(__name__)
 
 #database config
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://username:password@server/db'
-
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:Alittlesuger19@rm-bp1u570eyer5u59x1.mysql.rds.aliyuncs.com/train'
 
 #instantilize db
 db = SQLAlchemy(app)
@@ -15,3 +16,10 @@ db = SQLAlchemy(app)
 def hello():
     return "hello"
 
+@app.route('/test')
+def test():
+    sql = text("select * from `user`")
+    result = db.engine.execute(sql)
+    for row in result:
+        print(row)
+    return row
